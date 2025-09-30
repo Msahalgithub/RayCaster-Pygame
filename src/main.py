c506c9805687035858a905890e9d456ea55ae931
sahal <sahalchalukolappa@gmail.com>
2025-09-30 20:09:58 +0530

    Player implementation

--- src/main.py
+++ src/main.py
@@ -2,23 +2,22 @@ import pygame
 import sys
 from settings import *
 from map import Map
+from player import Player
 
 ()
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 game_map = Map()
-
+player_instance = Player()
 while True:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 sys.exit()
-    m = pygame.mouse.get_pos()
 
-    k = game_map.has_wall(m[0], m[1])
-
-    print(k)
     screen.fill("black")
     game_map.render(screen=screen)
+    player_instance.render(screen)
+
     pygame.display.update()
--- src/player.py
+++ src/player.py
@@ -1,2 +1,14 @@
 import pygame
 import math
+from settings import *
+
+
+class Player:
+    def __init__(self) -> None:
+        self.x = WIDTH / 2
+        self.y = HEIGHT / 2
+        self.radius = 15
+        self.color = "red"
+
+    def render(self, screen):
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
