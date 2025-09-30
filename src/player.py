fc1daa26a50bb1f99b499ebc96c90f5e11c94a75
sahal <sahalchalukolappa@gmail.com>
2025-09-30 20:18:44 +0530

    Player implementation

--- src/player.py
+++ src/player.py
@@ -9,6 +9,16 @@ class Player:
         self.y = HEIGHT / 2
         self.radius = 15
         self.color = "red"
+        self.rotationAngle = 50
 
     def render(self, screen):
         pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
+        pygame.draw.line(
+            screen,
+            "red",
+            (self.x, self.y),
+            (
+                self.x + math.cos(self.rotationAngle) * 50,
+                self.y + math.sin(self.rotationAngle) * 50,
+            ),
+        )
